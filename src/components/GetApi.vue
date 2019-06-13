c7f67f2f5e0805f23c20d2d555a6a66255c246b7
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-06-13 09:27:29 +0700

    ADD | Basic Axious Swapi Fetching

---
+++ src/components/GetApi.vue
@@ -0,0 +1,22 @@
+<template>
+    <tbody>
+        <tr>
+        <td>{{ film.episode_id }}</td>
+        <td>{{ film.title }}</td>
+        <!-- <td>{{ film.opening_crawl }}</td> -->
+        <td>{{ film.director }}</td>
+        <td>{{ film.release_date }}</td>
+        <!-- <td><a :href="film.url" target="_blank">{{film.url}}</a></td> -->
+        </tr>
+    </tbody>
+</template>
+
+<script>
+    export default {
+        name: 'GetApi',
+        props: {
+            film: Object,
+            index: Number
+        },
+    }
+</script>
